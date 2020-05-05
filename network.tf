0b95eba27c7df27a9ff1c22592d56d3c5f3246cb
robbytherobotibm <64825307+robbytherobotibm@users.noreply.github.com>
2020-05-05 15:20:25 +1000

    Update network.tf

--- network.tf
+++ network.tf
@@ -12,11 +12,11 @@ resource ibm_is_network_acl multizone_acl {
     for_each = "${var.acl_rules}"
 
     content {
-      name        = rules.value["name"]
-      action      = rules.value["action"]
-      source      = rules.value["source"]
-      destination = rules.value["destination"]
-      direction   = rules.value["direction"]
+      name        = "${rules.value.name}"
+      action      = "${rules.value.action}"
+      source      = "${rules.value.source}"
+      destination = "${rules.value.destination}"
+      direction   = "${rules.value.direction}"
     }
 
   }
